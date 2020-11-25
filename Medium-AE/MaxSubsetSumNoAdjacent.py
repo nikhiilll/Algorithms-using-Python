6baf897e24dbe65722165f14fb0e2938a5627ed3
nikhiilll <pawarnikhilp@gmail.com>
2020-11-25 21:56:27 +0530

    EOD 11/25/20

---
+++ Medium-AE/MaxSubsetSumNoAdjacent.py
@@ -0,0 +1,18 @@
+def maxSubsetSumNoAdjacent(array):
+    """
+    TC: O(n) | SC: O(1)
+    """
+    if not len(array):
+        return 0
+    elif len(array) == 1:
+        return array[0]
+
+    second = array[0]
+    first = max(array[0], array[1])
+
+    for i in range (2, len(array)):
+        current = max(first, second + array[i])
+        second = first
+        first = current
+    
+    return first
