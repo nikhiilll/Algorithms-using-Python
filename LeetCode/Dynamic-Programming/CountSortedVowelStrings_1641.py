1a66ed6b8092fead71a5a6cef8edea6d68adf241
nikhiilll <pawarnikhilp@gmail.com>
2020-11-27 22:58:48 +0530

    EOD 11/27/20

---
+++ LeetCode/Dynamic-Programming/CountSortedVowelStrings_1641.py
@@ -0,0 +1,9 @@
+def countSortedVowelStrings(n):
+
+    dp = [[1] * 6]+ [[0] * 6 for _ in range(n)]
+
+    for i in range(1, n + 1):
+        for k in range(1, 6):
+            dp[i][k] = dp[i][k - 1] + dp[i - 1][k]
+    
+    return dp[n][5]
